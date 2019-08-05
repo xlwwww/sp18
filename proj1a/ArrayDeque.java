6ff1798affac95a36cafcbd5359615fe1f3dfe4b
Wangxinlu <517431424@qq.com>
2019-08-05 12:32:08 +0800

    revised

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,34 +1,47 @@
 public class ArrayDeque<T> {
     private int size;
     private T [] array;
+    private int nextFirst;
+    private int nextLast;
     public ArrayDeque(){
         size=0;
         array = (T[])new Object [8];
+        nextFirst = 3;
+        nextLast = 3;
     }
     private void resize(int len){
         T[] a = (T[]) new Object[len];
-        System.arraycopy(array, 0, a, 0, size);
+        System.arraycopy(array, 0, a, 0, nextLast);
+        System.arraycopy(array,nextFirst+1,a,array.length+1+nextFirst,array.length-nextFirst-1);
+        nextFirst =array.length+nextFirst;
         array = a;
     }
 
     public void addFirst(T item){
-        if (size == array.length){
+        if (nextLast+1==nextFirst){
             resize(array.length*2);
-            System.out.println(array.length);
         }
-
-        T[] t = (T[]) new Object[array.length];
-        t[0] = item;
-        System.arraycopy(array,0,t,1,size);
+        array[nextFirst]=item;
         size +=1;
-        array = t;
+        if (size==1){
+            nextLast +=1;
+        }
+        nextFirst -=1;
     }
     public void addLast(T item){
-        if (size == array.length){
+        if (nextLast+1==nextFirst){
             resize(array.length*2);
         }
-        array[size] = item;
+        // ensure nextlast has space
+        array[nextLast] = item;
         size +=1;
+        if (size==1){
+            nextFirst -=1;
+        }
+        nextLast +=1;
+        if (nextLast>=array.length){
+            nextLast = nextLast%array.length;
+        }
     }
     public boolean isEmpty(){
         return size == 0;
@@ -37,9 +50,8 @@ public class ArrayDeque<T> {
         return size;
     }
     public void printDeque(){
-        for (int i =0;i<size;i++){
-            System.out.print(array[i]);
-            System.out.print(" ");
+        for (int i =nextFirst+1;i<nextFirst+size+1;i++){
+            System.out.print(array[i%array.length]+" ");
         }
         System.out.println();
     }
@@ -47,10 +59,9 @@ public class ArrayDeque<T> {
         if (size==0){
             return null;
         }
-        T tmp = array[0];
-        T[] a = (T[]) new Object[array.length];
-        System.arraycopy(array, 1, a, 0, size-1);
-        array = a;
+        T tmp = array[nextFirst+1];
+        array[nextFirst+1]=null;
+        nextFirst+=1;
         size -=1;
         float R = (float) size/array.length;
         if (R<0.25 &&array.length>16){
@@ -62,9 +73,13 @@ public class ArrayDeque<T> {
         if (size==0){
             return null;
         }
-        T tmp = array[size-1];
-        array[size-1]=null;
+        T tmp = array[nextLast-1];
+        array[nextLast-1]=null;
         size -=1;
+        nextLast -=1;
+        if (nextLast<0){
+            nextLast = array.length-1;
+        }
         float R = (float) size/array.length;
         if (R<0.25 &&array.length>16){
             resize(array.length/2);
@@ -75,7 +90,6 @@ public class ArrayDeque<T> {
         if (index>=size){
             return null;
         }
-        return array[index];
+        return array[(nextFirst+1+index)%array.length];
     }
-
 }
